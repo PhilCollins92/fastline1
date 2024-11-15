b493bb9919bdfd224fff979cfe3897646c0f4faa
philippe <philippe.pk57@gmail.com>
2024-11-15 16:22:29 +0100

    ajout des fichiers fastlist+docker

--- scripts/script-1.sh
+++ scripts/script-1.sh
@@ -1,47 +1,3 @@
-# # Variables
-# REPO_URL="https://github.com/PhilCollins92/fastline1.git"
-# APP_DIR="/var/www/monprojet"
-# BRANCH="main"
-
-# # Désactiver le site par défaut et activer celui de Symfony
-# a2dissite 000-default.conf
-# a2ensite symfony.conf
-# a2enmod rewrite
-
-# # Décompresser et installer l'application si le fichier zip existe
-# if [ -f /var/www/fastlist.zip ]; then
-#     echo "Décompression de fastlist.zip..."
-#     unzip /var/www/fastlist.zip -d /var/www/
-#     rm -f /var/www/fastlist.zip
-#     chown -R www-data:www-data /var/www/
-#     chmod -R 755 /var/www/
-# else
-#     echo "Erreur : fastlist.zip non trouvé!"
-#     exit 1
-# fi
-
-# # Installer Composer et gérer les dépendances
-# curl -sS https://getcomposer.org/installer | php
-# mv composer.phar /usr/local/bin/composer
-# composer --version
-# cd /var/www/fastlist || exit 1
-# composer install
-
-# # Modifier le fichier .env pour l'environnement de production
-# sed -i 's/APP_ENV=dev/APP_ENV=prod/' /var/www/fastlist/.env
-
-# # Vider le cache Symfony pour l'environnement de production
-# php /var/www/fastlist/bin/console cache:clear --no-warmup --env=prod
-
-# # Configurer les permissions
-# chmod -R 777 /var/www/fastlist/var/
-# echo "Permissions configurées pour /var/www/fastlist/var/"
-
-# # Vider le cache Symfony (pas besoin de redémarrer Apache dans Docker)
-# echo "Vider le cache Symfony..."
-# cd /var/www/fastlist/
-# php bin/console cache:clear
-
 #!/bin/bash
 # Variables
 REPO_URL="https://github.com/PhilCollins92/fastline1.git"
@@ -56,10 +12,6 @@ apt-get update && apt-get install -y \
     curl \
     php-cli \
     && apt-get clean
-
-# #Clone du projet
-# echo "clonage du projet"
-# git clone https://github.com/PhilCollins92/fastline1.git
 
 # Désactiver le site par défaut et activer celui de Symfony
 echo "Configuration d'Apache..."
@@ -97,6 +49,7 @@ if [ -f .env ]; then
 fi
 
 # Vider le cache Symfony pour l'environnement de production
+composer install
 
 echo "Vider le cache Symfony..."
 php bin/console cache:clear --no-warmup --env=prod
