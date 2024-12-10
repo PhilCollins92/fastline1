aa647a8c9013e20463a6dbaf484cb93af523419a
philippe <philippe.pk57@gmail.com>
2024-12-10 15:59:19 +0100

    fix(environnement) from test env to prod env

--- scripts/script-1.sh
+++ scripts/script-1.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Variables
-# REPO_URL="https://github.com/PhilCollins92/fastline1.git"
-# APP_DIR="/var/www/fastline1/"
-# BRANCH="main"
-
 # Mise à jour des paquets nécessaires
 echo "Mise à jour des paquets nécessaires..."
 apt-get update && apt-get install -y \
@@ -25,18 +20,6 @@ a2dissite 000-default.conf
 a2ensite symfony.conf
 a2enmod rewrite
 
-# # Cloner le dépôt depuis GitHub
-# if [ ! -d "$APP_DIR" ]; then
-#     echo "Clonage du dépôt Git..."
-#     git clone --branch "$BRANCH" "$REPO_URL" "$APP_DIR"
-#     chown -R www-data:www-data "$APP_DIR"
-#     chmod -R 755 "$APP_DIR"
-# else
-#     echo "Le dépôt existe déjà, mise à jour des fichiers..."
-#     cd "$APP_DIR" || exit 1
-#     git pull origin "$BRANCH"
-# fi
-
 # Installer Composer
 echo "Installation de Composer..."
 curl -sS https://getcomposer.org/installer | php
@@ -48,11 +31,9 @@ echo "Installation des dépendances Symfony..."
 cd "$APP_DIR" || exit 1
 composer install --no-dev --optimize-autoloader
 
-# Configurer l'environnement de production
+# Modifier le fichier .env pour l'environnement de production
 echo "Configuration de l'environnement Symfony..."
-if [ -f .env ]; then
-    sed -i 's/APP_ENV=dev/APP_ENV=prod/' .env
-fi
+sed -i 's/APP_ENV=dev/APP_ENV=prod/' /var/www/fastlist/.env
 
 # Vider le cache Symfony pour l'environnement de production
 composer install
